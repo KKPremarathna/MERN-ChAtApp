506657780dfe8d69a93f4074284ec8f3ebc5da4a
H.M.K.K.Premarathna <kavindukavishkacampus@gmail.com>
2025-10-20 11:21:11 +0530

    SocketUrl updated

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://chatapp-mern-gv6n.onrender.com", {
         query: {
           userId: authUser._id,
         },
